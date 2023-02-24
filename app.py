a9321f222c7d04eb1391861d79a4c7572375ef32
Jammie sandy <33603488+jamessandy@users.noreply.github.com>
2023-02-24 15:07:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,18 +44,36 @@ def prediction(image, model):
     Pred=cat[result]
     return Pred
 
+MODEL_PATH2 ='one-class.h5'
+model1 = load_model(MODEL_PATH2)
 
+def  leaf_predict(image, model1):
+    test_image = image.resize((200,200))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
+    preds = model1.predict(test_image)
+    dist = np.linalg.norm(test_image - preds)
+    if dist <= 20:
+        return "leaf"
+    else:
+        return "not leaf"
 
 if file is not None:
     img = Image.open(file)
     model = tf.keras.models.load_model("tomato_disease.h5")
     img_jpeg = img.convert('RGB')
-    pred = prediction(img_jpeg, model)
-    #score = tf.nn.softmax(prediction[0])
-    st.markdown(f"<h2 style='text-align: center; color: black;'>{pred}</h2>", unsafe_allow_html=True)
-    st.image(img, use_column_width=True)
+    leaf = leaf_predict(img_jpeg, model1)
+    if leaf == "leaf":
+        pred = prediction(img_jpeg, model)
+        #score = tf.nn.softmax(prediction[0])
+        st.markdown(f"<h2 style='text-align: center; color: black;'>{pred}</h2>", unsafe_allow_html=True)
+        st.image(img, use_column_width=True)
     
-
+     
+        
+   
+    
 
 
 
