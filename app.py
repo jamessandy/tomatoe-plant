69516697db57f150b42ee511bfffc34f198e832f
Jammie sandy <33603488+jamessandy@users.noreply.github.com>
2023-02-24 15:58:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def prediction(image, model):
 #model1 = load_model(MODEL_PATH2)
 
 def  leaf_predict(image, model1):
-    test_image = image.resize((200,200))
+    test_image = image.resize((224, 224))
     test_image = preprocessing.image.img_to_array(test_image)
     test_image = test_image / 255.0
     test_image = np.expand_dims(test_image, axis=0)
